ae2f2d84eb57ecb9b054ea9684a6b0dd6c36b6c1
Gustavo Esporte <esportegustavo@gmail.com>
2023-04-17 21:06:27 -0300

    testing

--- Chess_com_archive.py
+++ Chess_com_archive.py
@@ -24,16 +24,3 @@ def chess_matches(archive_url,player_name):
     print(f"All games of {player_name} exported!")
     os.chdir('../')
     return 
-def chess_matches(archive_url,player_name):
-    print("Games extraction started..")
-    df = []
-    for matches in archive_url:
-        matches = pd.json_normalize(requests.get(matches).json()["games"])
-        matches = matches.squeeze()
-        df.append(matches)
-        df_final = pd.concat(df)
-    os.chdir('./raw_chess_players_data')
-    df_final.to_excel(f'{player_name}_archive.xlsx')
-    print(f"All games of {player_name} exported!")
-    os.chdir('../')
-    return 
