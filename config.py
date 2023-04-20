f2fab309f3534a9fb63277fe854721f7745021a1
Gustavo Esporte dos Santos <esportegustavo@gmail.com>
2023-04-20 15:13:16 -0300

    clean df_final on pos processing

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 chess_players = [
     "lpsupi"
-    #,"GMKrikor"
-    #,"GMRafpig"
+    ,"GMKrikor"
+    ,"GMRafpig"
 ]
+
+pgn_columns = ["Event", "Site", "Date", "Round", "White", "Black", "Result", "CurrentPosition", "Timezone", "ECO", "ECOUrl", "UTCDate", "UTCTime", "WhiteElo", "BlackElo", "TimeControl", "Termination", "StartTime", "EndDate", "EndTime", "Link", "match id"]
+
